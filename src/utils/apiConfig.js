5962bb96f3be3139851934803eb052ba01c2be55
Liza <liza@gonano.com>
2025-07-09 12:58:59 +0530

    1.3

--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -1,6 +1,6 @@
 export const getApiUrl = () => {
   const currentUrl = window.location.href;
   return currentUrl.includes('192.168') 
-    ? 'https://sc-chat-backend.onrender.com/'
-    : 'https://sc-chat-backend.onrender.com/';
+    ? 'https://sc-chat-backend.onrender.com'
+    : 'https://sc-chat-backend.onrender.com';
 }; 
